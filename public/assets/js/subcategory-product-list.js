0094de1795a9d799a6125b43ca1796540c347b93
palashsamantalnsel <palashsamanta.lnsel@gmail.com>
2024-01-09 16:13:28 +0530

    update latest code

--- public/assets/js/subcategory-product-list.js
+++ public/assets/js/subcategory-product-list.js
@@ -1,3 +1,12 @@
 $(document).ready(async function() {
-    
+    $(document).on("click",".filterByChecked",async function() {
+        let thisId = $(this).data("id");
+        let brandList = [];
+        let sizeList = [];
+        let conditionList = [];
+        let priceList = [];
+        if($(this).hasClass("searchByBrand")) {
+
+        }
+    });
 });

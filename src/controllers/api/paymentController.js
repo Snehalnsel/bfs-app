5f4345bd091bb6697ca19ec365e04c0c76c183cd
soug <souravgupta.lnsel@gmail.com>
2024-04-03 15:47:35 +0530

    checking paymenrt

--- src/controllers/api/paymentController.js
+++ src/controllers/api/paymentController.js
@@ -48,8 +48,10 @@ const uniqid = require("uniqid");
 const MERCHANT_ID = "PGTESTPAYUAT";
 const PHONE_PE_HOST_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox";
 const SALT_INDEX = 1;
-const SALT_KEY = "099eb0cd-02cf-4e2a-8aca-3e6c6aff0399";
+// const SALT_KEY = "099eb0cd-02cf-4e2a-8aca-3e6c6aff0399";
+const SALT_KEY = "6e2f6cdb-392f-4a06-b2e2-a9af19a1207c";
 const APP_BE_URL = process.env.SITE_URL;
+//6e2f6cdb-392f-4a06-b2e2-a9af19a1207c
 
 exports.getPaymentData = async function (req, res, next) {
   try {

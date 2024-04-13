2a7a17d53660da6bd642590f5a3a5631ab9ea1a0
soug <souravgupta.lnsel@gmail.com>
2024-04-13 14:47:12 +0530

    images order in user-filter

--- src/controllers/api/websiteController.js
+++ src/controllers/api/websiteController.js
@@ -987,7 +987,19 @@ exports.userFilter = async function (req, res, next) {
     .limit(pageSize);
   const formattedUserProducts = [];
   for (const userproduct of allProductData) {
-    const productImages = await Productimage.find({ product_id: userproduct._id });
+    // const productImages = await Productimage.find({ product_id: userproduct._id });
+    const productImages = await Productimage.aggregate([
+      {
+        $match: {
+          product_id: userproduct._id,
+          $or: [
+            { image_order: 1 },
+            { image_order: 0 }
+          ]
+        }
+      }
+    ]).exec();
+    
     const productCondition = await Productcondition.findById(userproduct.status);
     const formattedUserProduct = {
       _id: userproduct._id,

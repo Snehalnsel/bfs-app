11eb884fcb11b6d4bd95c9cea74072b2b8bc6e0f
rabilnsel <rabi.lnsel@gmail.com>
2024-04-17 10:04:10 +0530

    payment amount updated

--- src/controllers/api/paymentController.js
+++ src/controllers/api/paymentController.js
@@ -73,7 +73,7 @@ exports.getPaymentData = async function (req, res, next) {
     {
       amount = parseFloat(temporder.booking_amount);
     }
-    amount = 1;
+    //amount = 1;
    // amount = temporder.booking_amount !== 0 ? temporder.booking_amount : temporder.total_price;
 
     let userId = temporder.user_id;

18d8a7e5220c35d1204a750ff65d1b76e1d6c29b
rabilnsel <rabi.lnsel@gmail.com>
2024-06-25 15:38:54 +0530

    updated

--- src/models/api/insertNotification.js
+++ src/models/api/insertNotification.js
@@ -1,18 +1,21 @@
 const Notifications = require("../../models/api/notificationModel");
 const mongoose = require("mongoose");
 const moment = require("moment");
-const dateTime = moment().format("YYYY-MM-DD h:mm:ss");
+// const dateTime = moment().format("YYYY-MM-DD h:mm:ss");
+
 const insertNotification = async (title, content, userId, link, addedDtime) => {
+    const dateTime = moment().tz('Asia/Kolkata').format('YYYY-MM-DD HH:mm:ss');
     try {
-        const notification = new Notifications({
+        let serObj = {
             title: title,
             content: content,
             user_id: userId,
             link: link,
             added_dtime: dateTime
-        });
-
+        };
+        const notification = new Notifications(serObj)
         await notification.save();
+
         return true;
     } catch (error) {
         return false;
